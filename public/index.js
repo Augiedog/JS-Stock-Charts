92c94daed620c8b2acb15ed33edec12a83291fe9
Augie Bren <Augie>
2022-04-16 20:12:33 -0500

    first chart

--- public/index.js
+++ public/index.js
@@ -1,10 +1,25 @@
+function getColor(stock){
+    if(stock === "GME"){
+        return '#32cf29'
+    }
+    if(stock === "MSFT"){
+        return '#ff0303'
+    }
+    if(stock === "DIS"){
+        return '#1822ab'
+    }
+    if(stock === "BNTX"){
+        return '#bab40b'
+    }
+}
+
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
     
-    const response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=1b5fe57d377f4f6f9cf09c643d6d05f9')
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=b2b0e11651294ee9a789558a1625b754`)
     const result = await response.json()
 
     const { GME, MSFT, DIS, BNTX } = result;
@@ -13,18 +28,19 @@ async function main() {
     stocks.forEach( stock => stock.values.reverse())
 
 
-new Chart(timeChartCanvas.getContext('2d'), {
-    type: 'line',
-    data: {
-        labels: stocks[0].values.map(value => value.datetime),
-        // datasets: stocks.map(stock => ({
-        //     label: stock.meta.symbol,
-        //     backgroundColor: getColor(stock.meta.symbol),
-        //     borderColor: getColor(stock.meta.symbol),
-        //     data: stock.values.map(value => parseFloat(value.high))
-        // }))
-    }
-});
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+                data: stock.values.map(value => parseFloat(value.high))
+            }))
+        }
+    });
+
 }
 
 main()
